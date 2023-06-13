e8adc114ea04080834fb2e5e18164ea320fd2b05
ekamei22 <67535236+ekamei22@users.noreply.github.com>
2023-06-13 18:37:42 +0700

    Add files via upload

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * The list of the inputs that are never flashed to the session on validation exceptions.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array
+     */
+    protected $dontFlash = [
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+    * Register the exception handling callbacks for the application.
+    *
+    * @return void
+    */
+   public function register()
+   {
+       $this->reportable(function (Throwable $e) {
+           //
+       });
+   }
+}
